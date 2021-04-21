bc3af4d3437214839ae0dc20ce05f22235785775
sam <samuncle@users.noreply.github.com>
2021-04-21 07:44:12 +0200

    prototype for weather

--- nodes/weather.gd
+++ nodes/weather.gd
@@ -1,12 +1,29 @@
 extends Node
 
-enum seasons {CLEAR, SNOW, RAIN, STORM}
+enum weather {CLEAR, SNOW, RAIN, STORM}
+
+var current_weather
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	current_weather = weather.CLEAR
 
+func _process(delta):
+	pick_next_weather()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func pick_next_weather():
+	randomize()
+	var weather_probability = randi()%100+1
+	
+	# By default the weather is clear
+	var new_weather = weather.CLEAR
+
+	if G.time.get_season() == G.time.seasons.SPRING:
+		# Spring has ~40% chances of rain
+		if weather_probability < 40:
+			new_weather = weather.RAIN
+		
+		# It is very very rare but sometimes it can snow during the first 3 days of spring
+		#if 
+	
+	#if G.time.get_season() == G.time.seasons.SPRING:
