c9edae5e38fd72da785fd1172e2335f214cc29fc
sam <samuncle@users.noreply.github.com>
2021-06-15 07:37:35 +0200

    Add better road sign

--- scripts/RoadSign.gd
+++ scripts/RoadSign.gd
@@ -1,34 +1,26 @@
 extends Node2D
 
-export var instructions = "REPLACE WITH INSTRUCTIONS"
-export var sign_text = "LOCATION"
+var instructions = "Press 'E' to read"
+export var sign_text_left = ""
+export var sign_text_right = ""
 export var is_interactive = true
-
-enum ICON {left, right, none}
-export (ICON) var sign_icon = ICON.none
-
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if len(sign_text) > 14 and sign_icon == ICON.none:
-		print("WARNING Text too long for sign post: ", name)
-		print("WARNING [", sign_text, "] might not display properly")
-	if len(sign_text) > 12 and sign_icon != ICON.none:
-		print("WARNING Text too long for sign post: ", name)
-		print("WARNING [", sign_text, "] might not display properly")
+	if len(sign_text_left) > 12 or len(sign_text_left) == 0:
+		print("WARNING Text too long or non existant for left sign post: ", name)
+		print("WARNING [", sign_text_left, "] might not display properly")
+	if len(sign_text_right) > 12 or len(sign_text_right) == 0:
+		print("WARNING Text too long or non existant for right sign post: ", name)
+		print("WARNING [", sign_text_right, "] might not display properly")
 	$SignInfo.hide()
-	$SignInfo/Label.text = sign_text
-	$SignInfo/ArrowRight.hide()
-	$SignInfo/ArrowLeft.hide()
-	if sign_icon == ICON.left:
-		$SignInfo/ArrowLeft.show()
-	if sign_icon == ICON.right:
-		$SignInfo/ArrowRight.show()
+	$SignInfo/LabelLeft.text = sign_text_left
+	$SignInfo/LabelRight.text = sign_text_right
 
 func action():
 	$SignInfo.show()
-	yield(get_tree().create_timer(3), "timeout")
+	yield(get_tree().create_timer(5), "timeout")
 	$SignInfo.hide()
 	$Area2D.update_instructions()
 
